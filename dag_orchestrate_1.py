0978cee0b7f0a0cee5feb4613edd66829d801417
CharvakDChavare <charvakchaware@gmail.com>
2026-09-17 17:56:57 +0530

    Add files via upload

---
+++ dag_orchestrate_1.py
@@ -0,0 +1,29 @@
+from airflow.sdk import dag, task
+import os
+@dag
+
+def first_orchestrateor_dag():
+
+    @task.python
+    def first_task():
+        print("thsi is first task")
+
+    @task.python
+    def second_task():
+        print("thsi is second task")
+
+    @task.python
+    def third_task():
+        os.makedirs(os.path.dirname("/opt/airflow/logs/data/output_1.txt"), exist_ok=True )
+
+        with open("/opt/airflow/logs/data/output_1.txt", 'w') as f:
+            f.write(f"Data fecthed successfully") 
+
+
+    first = first_task()
+    second = second_task()
+    third = third_task()
+
+    first >> second >> third
+
+first_orchestrateor_dag()
